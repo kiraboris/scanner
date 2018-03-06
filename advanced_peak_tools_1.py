3327d997a4baf03a4e982393aaccc11d03f15847
Kirill B <borisov@ph1.uni-koeln.de>
2018-03-06 14:06:34 +0100

    Imporved peak fitting

--- advanced_peak_tools_1.py
+++ advanced_peak_tools_1.py
@@ -1,73 +1,95 @@
 
 import matplotlib.pyplot as plt
-from collections import namedtuple
 import numpy as np
 import sys
 
-import pint
-import lmfit
 import lmfit.models as lmfit_models
 
-from basetypes import Ranges
+from basetypes import Ranges, Units, DIM 
 
 
-class LineProfileSettings: pass
+def step_and_span(settings, step_nw =2.0, span_nw = 4.0):
     
-
-def step_and_span(settings, step_nw =1.0, span_nw = 4.0):
-    
-    fwhm = settings.fwhm_estimate.to(settings.data_units).magnitude
-    
-    step = step_nw * fwhm
-    span = span_nw * fwhm
+    step = step_nw * settings.min_fwhm.to(settings.data_units).magnitude
+    span = span_nw * settings.max_fwhm.to(settings.data_units).magnitude
     
     return step, span
     
 
-def find_peaks(data, settings):
+def find_peaks(data_ranges, settings, fev_per_epoch = 16, nepochs=8):
     
-    data_ranges = Ranges(arrays=[data])
+    peak_model = lmfit_models.PseudoVoigtModel()     # TODO: derivatives
+    local_baseline_model = lmfit_models.LinearModel()
+    biased_peak_model = peak_model + local_baseline_model
     
-    # TODO: take account of derivatives
-    peak_model = lmfit_models.PseudoVoigtModel()
-    base_model = lmfit_models.LinearModel()
-    model = peak_model + base_model
+    nslices = data_ranges.nslices(*step_and_span(settings), dim=DIM.X)
+    slices  = data_ranges.slices(*step_and_span(settings), dim=DIM.X)
     
-    peaklist = []
+    # fidelity test for peak candidates 
+    def accept_peak(peak):
+        flag1 = np.max(peak.best_fit) - np.min(peak.best_fit) > 2 * np.std(yyy)
+        flag2 = (peak.params['fwhm'] > 
+            settings.resolution.to(settings.data_units).magnitude)
+            
+        return flag1 and flag2
+    
+    def converged_peak(peak):
+        return peak.ier in range(1, 5)  
+        # see https://docs.scipy.org/doc/scipy/reference/
+        #    generated/scipy.optimize.leastsq.html 
+
+    peaklist = []    
     print('Searching for peaks...')
-    nslices = data_ranges.nslices(*step_and_span(settings), dim=0)
-    slices  = data_ranges.slices(*step_and_span(settings), dim=0)
-    
     for i, d in enumerate(slices):
         offset = d[0, 0]
-        xxx = d[:, 0] - d[0, 0]
+        xxx = d[:, 0] - offset
         yyy = d[:, 1]
-                        
+        
+        # initital peak guess             
         params = peak_model.guess(yyy, x=xxx)
         params.add('intercept', value=0.0)
         params.add('slope', value=0.0)
-        fit_out = model.fit(yyy, params, 
-                            x=xxx, fit_kws={'maxfev': 50})
-                            
+
+        # peak fitting loop
+        scipy_leastsq_settings = {
+            'maxfev': fev_per_epoch,
+            'ftol':   1e-5,
+            'xtol':   1e-5,
+        }
+
+        for n in range(0, nepochs):
+            fit_out = biased_peak_model.fit(yyy, params, x = xxx,
+                                            fit_kws = scipy_leastsq_settings)
+            params = fit_out.params
+            if converged_peak(fit_out): 
+                break
+            if not accept_peak(fit_out):
+                break
+
+        
+        # result of peak guess and fit
+        if accept_peak(fit_out):
+            fit_out.xxx = xxx + offset
+            peaklist.append(fit_out)
+
+                                
         print("%i%%" % int(i / float(nslices) * 100), end='\r')
         sys.stdout.flush()
         
-        if fit_out.params['height'] > np.std(yyy):
-            fit_out.xxx = xxx + offset
-            peaklist.append(fit_out)
-        
-    return peaklist, model
+    return peaklist
         
 
 def test():
-    # test example for emission spectra
+    """test example for emission spectra"""
     
-    units = pint.UnitRegistry('units.txt')
+    units = Units.spec_units()
     
+    class LineProfileSettings: pass
     settings = LineProfileSettings() 
-    
     settings.data_units       = units.GHz
-    settings.fwhm_estimate    = 0.5 * units.MHz
+    settings.min_fwhm         = 0.2 * units.MHz
+    settings.max_fwhm         = 0.8 * units.MHz
+    settings.resolution       = 70 * units.kHz
     settings.derivative_order = 0
     
     folder = "/home/borisov/projects/work/emission/simple_model/"
@@ -75,8 +97,12 @@ def test():
     
     data = np.loadtxt(filename)[4000:6500, :]
     
-    peaklist, model = find_peaks(data, settings)
+    data_ranges = Ranges(arrays=[data])
     
+    peaklist = find_peaks(data_ranges, settings)
+    
+    
+    # output
     peaklist_sorted = sorted(peaklist, 
         key=lambda p: float(p.params['amplitude']), reverse=True)
 
--- basetypes.py
+++ basetypes.py
@@ -2,6 +2,56 @@
 #
 
 import numpy as np
+import pint
+
+class DIM:
+    """np.array dimensions"""
+    X = 0
+    Y = 1
+
+
+class Units:
+    
+    @staticmethod
+    def prefixes():
+        return [
+            'yocto- = 1e-24 = y-',
+            'zepto- = 1e-21 = z-',
+            'atto- =  1e-18 = a-',
+            'femto- = 1e-15 = f-',
+            'pico- =  1e-12 = p-',
+            'nano- =  1e-9  = n-',
+            'micro- = 1e-6  = u- = µ-',
+            'milli- = 1e-3  = m-',
+            'centi- = 1e-2  = c-',
+            'deci- =  1e-1  = d-',
+            'deca- =  1e+1  = da- = deka',
+            'hecto- = 1e2   = h-',
+            'kilo- =  1e3   = k-',
+            'mega- =  1e6   = M-',
+            'giga- =  1e9   = G-',
+            'tera- =  1e12  = T-',
+            'peta- =  1e15  = P-',
+            'exa- =   1e18  = E-',
+            'zetta- = 1e21  = Z-',
+            'yotta- = 1e24  = Y-'
+        ]
+    
+    @staticmethod
+    def spec_units():
+        lst_units = ([
+            'second = [time] = s',
+            'hertz = 1 / s = Hz',
+            'meter = s / 299792458 = m',
+            'wavenumber = 1 / cm = wn',
+        ] 
+        + Units.prefixes())
+        
+        units = pint.UnitRegistry(None)
+        units.load_definitions(lst_units)
+        
+        return units
+
 
 class Ranges:
     
@@ -10,12 +60,13 @@ class Ranges:
 
         self.__arrs = arrays
     
-    def max_height(self, dim=None):
+    def height(self, dim=None):
         
         if dim is None:
-            return max([max(a) - min(a) for a in self.__arrs])
+            return max([np.max(a) - np.min(a) for a in self.__arrs])
         else:
-            return max([max(a[:, dim]) - min(a[:, dim]) for a in self.__arrs])
+            return max([np.max(a[:, dim]) - np.min(a[:, dim]) 
+                for a in self.__arrs])
             
     def nslices(self, step, span, dim=None):
         
