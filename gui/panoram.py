1e7e903ee8cc685a98cea1e92aee68a775f6e6cb
Kirill Borisov <borisov@ph1.uni-koeln.de>
2018-07-25 17:41:44 -0700

    GUI: --

---
+++ gui/panoram.py
@@ -0,0 +1,58 @@
+
+from . import pyqtgraph as pg
+
+class Panoram(pg.GraphicsLayoutWidget):
+    def __init__(self, parent=None):
+        pg.GraphicsLayoutWidget.__init__(self, parent)
+
+        self.__p1 = self.addPlot(row=1, col=0)
+        self.__p2 = self.addPlot(row=2, col=0)
+        self.__p3 = self.addPlot(row=3, col=0)
+
+        self.centralWidget.layout.setRowStretchFactor(2, 3)
+        self.centralWidget.layout.setRowStretchFactor(3, 3)
+
+        self.region = pg.LinearRegionItem()
+        self.region.setZValue(10)
+        self.region.setMovable(m=True, lm=False)
+        self.region.sigRegionChanged.connect(self._updateRanges)
+        self.__p1.addItem(self.region, ignoreBounds=True)
+
+        self._setupMasterPlot(self.__p1)
+        self._setupSlavePlot(self.__p2)
+        self._setupSlavePlot(self.__p3)
+
+    def _setupSlavePlot(self, plot):
+        plot.setAutoVisible(y=True)
+        plot.hideButtons()
+        plot.setMenuEnabled(enableMenu=False, enableViewBoxMenu=True)
+        plot.sigRangeChanged.connect(self._updateRegion)
+
+    def _setupMasterPlot(self, plot):
+        plot.hideButtons()
+        plot.setMenuEnabled(enableMenu=False, enableViewBoxMenu=True)
+        plot.getViewBox().sigMouseClick.connect(self._setRegionCenter)
+
+    def plotUpper(self, *args, **kwargs):
+        self.__p1.plot(args, kwargs)
+        self.__p2.plot(args, kwargs)
+
+    def plotLower(self, *args, **kwargs):
+        self.__p3.plot(args, kwargs)
+
+    def _setRegionCenter(self, pos):
+        minX, maxX = self.region.getRegion()
+        width = maxX - minX
+        newRegion = (pos.x() - width / 2, pos.x() + width / 2)
+        self.region.setRegion(newRegion)
+
+    def _updateRanges(self):
+        self.region.setZValue(10)
+        minX, maxX = self.region.getRegion()
+        self.__p1.setXRange(minX, maxX, padding=0)
+        self.__p2.setXRange(minX, maxX, padding=0)
+        self.__p3.setXRange(minX, maxX, padding=0)
+
+    def _updateRegion(self, window, viewRange):
+        rgn = viewRange[0]
+        self.region.setRegion(rgn)
--- gui/scanner.py
+++ gui/scanner.py
@@ -1,118 +1,50 @@
-"""
-Demonstrates some customized mouse interaction by drawing a crosshair that follows 
-the mouse.
 
-
-"""
-
-#import initExample ## Add path to library (just for examples; you do not need this)
 import numpy as np
+from .pyqtgraph.Qt import QtGui
+
 from . import pyqtgraph as pg
-from .pyqtgraph.Qt import QtGui, QtCore
+from . import panoram
 
-#pg.setConfigOption('background', 'w')
-
-#generate layout
 app = QtGui.QApplication([])
-win = pg.GraphicsWindow(size=(1280, 720))
-win.setWindowTitle('scanner')
+win = QtGui.QWindow()
+win.setTitle('scanner')
+
+pan = panoram.Panoram()
 label = pg.LabelItem(justify='right')
-
-p1 = win.addPlot(row=2, col=0)
-p2 = win.addPlot(row=1, col=0)
-p0 = win.addPlot(row=3, col=0)
-
-p0.hideButtons()
-p1.hideButtons()
-p2.hideButtons()
-p1.setMenuEnabled(enableMenu=False, enableViewBoxMenu=True)
-p2.setMenuEnabled(enableMenu=False, enableViewBoxMenu=True)
-p0.setMenuEnabled(enableMenu=False, enableViewBoxMenu=True)
-
-win.centralWidget.addItem(label, row=4, col=0)
-win.centralWidget.layout.setRowStretchFactor(2,3)
-win.centralWidget.layout.setRowStretchFactor(3,3)
-
-
-region = pg.LinearRegionItem()
-region.setZValue(10)
-region.setMovable(m=True, lm=False)
-
-
-# Add the LinearRegionItem to the ViewBox, but tell the ViewBox to exclude this 
-# item when doing auto-range calculations.
-p2.addItem(region, ignoreBounds=True)
-
-#pg.dbg()
-p1.setAutoVisible(y=True)
-p0.setAutoVisible(y=True)
-#p1.setMouseEnabled(True)
-
-
 
 #create numpy arrays
 #make the numbers large to show that the xrange shows data from 10000 to all the way 0
 data = 10000 + 15000 * pg.gaussianFilter(np.random.random(size=10000), 10) + 3000 * np.random.random(size=10000)
 
-p1.plot(data, pen="m")
+pan.plotUpper(data, pen="m")
+pan.plotLower(data, pen="0ff")
 
-p2.plot(data, pen="m")
-
-p0.plot(data, pen="0ff")
-
-#p2.autoRange(True)
-
-def update():
-    region.setZValue(10)
-    minX, maxX = region.getRegion()
-    p1.setXRange(minX, maxX, padding=0)
-    p0.setXRange(minX, maxX, padding=0)
-
-
-region.sigRegionChanged.connect(update)
-
-def updateRegion(window, viewRange):
-    rgn = viewRange[0]
-    region.setRegion(rgn)
-
-def setRegionCenter(pos):
-    minX, maxX = region.getRegion()
-    width = maxX - minX
-    newRegion = (pos.x() - width / 2, pos.x() + width / 2)
-    region.setRegion(newRegion)
-
-p0.sigRangeChanged.connect(updateRegion)
-p1.sigRangeChanged.connect(updateRegion)
-p2.getViewBox().sigMouseClick.connect(setRegionCenter)
-
-
-
-region.setRegion([1800, 2000])
+pan.region.setRegion([1800, 2000])
 
 #cross hair
-vLine = pg.InfiniteLine(angle=90, movable=False)
-hLine = pg.InfiniteLine(angle=0, movable=False)
+#vLine = pg.InfiniteLine(angle=90, movable=False)
+#hLine = pg.InfiniteLine(angle=0, movable=False)
 #p1.addItem(vLine, ignoreBounds=True)
 #p1.addItem(hLine, ignoreBounds=True)
 
 
-vb = p1.getViewBox()
+#vb = p1.getViewBox()
 
 
-def mouseMoved(evt):
-    pos = evt[0]  ## using signal proxy turns original arguments into a tuple
-    if p1.sceneBoundingRect().contains(pos):
-        mousePoint = vb.mapSceneToView(pos)
-        index = int(np.round(mousePoint.x()))
-        if index > 0 and index < len(data):
-            label.setText("<span style='font-size: 12pt'>Frequency=%0.2f, "
-                          "  <span style='color: magenta'>Intensity=%0.2f</span>"
-                          % (mousePoint.x(), data[index]))
+#def mouseMoved(evt):
+#    pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+#    if p1.sceneBoundingRect().contains(pos):
+#        mousePoint = vb.mapSceneToView(pos)
+#        index = int(np.round(mousePoint.x()))
+#        if index > 0 and index < len(data):
+#            label.setText("<span style='font-size: 12pt'>Frequency=%0.2f, "
+#                          "  <span style='color: magenta'>Intensity=%0.2f</span>"
+#                          % (mousePoint.x(), data[index]))
         #vLine.setPos(mousePoint.x())
         #hLine.setPos(mousePoint.y())
 
 
 
-proxy = pg.SignalProxy(p1.scene().sigMouseMoved, rateLimit=60, slot=mouseMoved)
+#proxy = pg.SignalProxy(p1.scene().sigMouseMoved, rateLimit=60, slot=mouseMoved)
 #p1.scene().sigMouseMoved.connect(mouseMoved)
 
